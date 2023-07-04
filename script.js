d01f52df4b8db143bf1a3b4356b2ce864cc9a2d9
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-07-04 15:52:08 -0500

    working local storage

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var today = dayjs();
+console.log(dayjs().format('h'));
+console.log(dayjs().hour());
 var currentHour = today.hour();
 
 var timeBlockEl = $(".time-block");
 var saveButtonEl = $(".saveBtn");
-var timeBlockSave = [];
 
 // Shorthand for $(document).ready(function() { ... });
 // Ensures your functions are called once all the DOM elements have finished rendering.
@@ -28,15 +29,19 @@ $(function () {
     } else {
       $(timeBlockEl[i]).addClass('future');
     }
+
+    var getBlockId = $(timeBlockEl[i]).attr("id")
+    console.log(getBlockId);
+
+    
+    var storedData = JSON.parse(localStorage.getItem(getBlockId))
+
+    if (storedData !== null) {
+      timeBlockEl[i].children[1].append(storedData);
+    }
   }
-
-  // localStorage.getItem()
-  // var storedSchedule = JSON.parse(localStorage.getitem(""));
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
 
   // XXXXXXXXXXXXTODO: Add code to display the current date in the header of the page.
   $("#currentDay").text(today.format("dddd, MMMM D"));
+
 });
